cd92216525cfc27b07c05f319935483d0c1c1806
Javier Berrocal <jberolm@unex.es>
2023-12-17 15:59:11 +0100

    Fallo versión Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.firstproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.firstproject"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
